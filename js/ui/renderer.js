08f20f803671703b1300525d2a76fe4bf3d251a2
coding-guys99 <tnuocsa2019@gmail.com>
2026-04-03 12:17:37 +0800

    Update renderer.js

--- js/ui/renderer.js
+++ js/ui/renderer.js
@@ -1,13 +1,12 @@
 import { state } from "../core/state.js";
 import { VIEW_TYPES } from "../core/constants.js";
-
 import { renderDashboardView } from "./views/dashboard.view.js";
 import { renderHistoryView } from "./views/history.view.js";
 import { renderReportsView } from "./views/reports.view.js";
 import { renderLeaveView } from "./views/leave.view.js";
 import { renderNotificationsView } from "./views/notifications.view.js";
 import { renderAnnouncementsView } from "./views/announcements.view.js";
-import { renderAdminView, bindAdminActions } from "./views/admin.view.js";
+import { renderAdminView } from "./views/admin.view.js";
 import { renderSettingsView } from "./views/settings.view.js";
 
 export function renderApp() {
@@ -30,19 +29,17 @@ export function renderApp() {
   }
 
   if (state.currentView === VIEW_TYPES.NOTIFICATIONS) {
-    container.innerHTML = renderNotificationsView();
+    container.innerHTML = renderNotificationsView(state.notifications);
     return;
   }
 
   if (state.currentView === VIEW_TYPES.ANNOUNCEMENTS) {
-    container.innerHTML = renderAnnouncementsView();
+    container.innerHTML = renderAnnouncementsView(state.announcements);
     return;
   }
 
   if (state.currentView === VIEW_TYPES.ADMIN) {
     container.innerHTML = renderAdminView();
-    bindAdminActions();
-
     return;
   }
 
