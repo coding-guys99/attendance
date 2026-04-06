ab38391a4ed388c4b91fa9c76f5053fbb06dd898
coding-guys99 <tnuocsa2019@gmail.com>
2026-04-06 11:00:43 +0800

    Create countries.js

---
+++ js/core/countries.js
@@ -0,0 +1,7 @@
+export const COUNTRY_OPTIONS = [
+  { code: "TW", label: "台灣" },
+  { code: "MY", label: "馬來西亞" },
+  { code: "JP", label: "日本" },
+  { code: "SG", label: "新加坡" },
+  { code: "US", label: "美國" },
+];
